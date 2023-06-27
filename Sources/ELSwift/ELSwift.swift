b2c8a4b5f31c05c2109005f5affe18ead83da210
SUGIMURA Hiroshi <hiroshi.sugimura@gmail.com>
2023-06-28 08:16:40 +0900

    init

---
+++ Sources/ELSwift/ELSwift.swift
@@ -0,0 +1,6 @@
+public struct ELSwift {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
---
+++ Tests/ELSwiftTests/ELSwiftTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import ELSwift
+
+final class ELSwiftTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(ELSwift().text, "Hello, World!")
+    }
+}
