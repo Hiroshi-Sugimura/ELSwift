7cb19f218b25623d6a9e0617ad02d0fa464a8f89
sugimura@katana <hiroshi.sugimura@gmail.com>
2023-07-13 20:32:37 +0900

    DocC

--- Package.swift
+++ Package.swift
@@ -17,6 +17,7 @@ let package = Package(
             targets: ["ELSwift"]),
     ],
     dependencies: [
+        .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.0.0"),
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
     ],
